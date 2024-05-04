2f320678a994cf7acfe17f3025bcb60bdcca47ba
Shubhashis Pradhan <iamshubhashis@gmail.com>
2024-05-04 19:34:25 +0530

    prediction done website local host docker

--- cellSegmentation/pipeline/training_pipeline.py
+++ cellSegmentation/pipeline/training_pipeline.py
@@ -1,12 +1,13 @@
 import sys,os
 from cellSegmentation.components.data_validation import DataValidation
+from cellSegmentation.components.model_trainer import ModelTrainer
 from cellSegmentation.logger import logging
 from cellSegmentation.exception import AppException
 from cellSegmentation.components.data_ingestion import DataIngestion
 
-from cellSegmentation.entity.config_entity import (DataIngestionConfig, DataValidationConfig)
+from cellSegmentation.entity.config_entity import (DataIngestionConfig, DataValidationConfig,ModelTrainerConfig)
 
-from cellSegmentation.entity.artifacts_entity import (DataIngestionArtifact, DataValidationArtifact)
+from cellSegmentation.entity.artifacts_entity import (DataIngestionArtifact, DataValidationArtifact,ModelTrainerArtifact)
 
 
 
@@ -14,7 +15,7 @@ class TrainPipeline:
     def __init__(self):
         self.data_ingestion_config = DataIngestionConfig()
         self.data_validation_config = DataValidationConfig()
-        #self.model_trainer_config = ModelTrainerConfig()
+        self.model_trainer_config = ModelTrainerConfig()
 
 
     
@@ -62,13 +63,25 @@ class TrainPipeline:
         except Exception as e:
             raise AppException(e, sys) from e
         
+    def start_model_trainer(self) -> ModelTrainerArtifact:
+        try:
+            model_trainer = ModelTrainer(
+                model_trainer_config=self.model_trainer_config,
+            )
+            model_trainer_artifact = model_trainer.initiate_model_trainer()
+            return model_trainer_artifact
+
+        except Exception as e:
+            raise AppException(e, sys)
+        
+
+        
     def run_pipeline(self) -> None:
         try:
             data_ingestion_artifact = self.start_data_ingestion()
-            data_validation_artifact = self.start_data_validation(
-                data_ingestion_artifact=data_ingestion_artifact
-            )
-        
+            data_validation_artifact = self.start_data_validation(data_ingestion_artifact=data_ingestion_artifact)
+            if data_validation_artifact.validation_status == True:
+                model_trainer_artifact = self.start_model_trainer()
         except Exception as e:
             raise AppException(e, sys)
         
