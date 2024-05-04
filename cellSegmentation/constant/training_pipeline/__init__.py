94dc1cb6d7bc031b79c9d258a235c8e899519ad1
Shubhashis Pradhan <iamshubhashis@gmail.com>
2024-05-04 17:56:36 +0530

    data validation done

--- cellSegmentation/constant/training_pipeline/__init__.py
+++ cellSegmentation/constant/training_pipeline/__init__.py
@@ -8,3 +8,16 @@ DATA_INGESTION_DIR_NAME: str = "data_ingestion"
 DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
 
 DATA_DOWNLOAD_URL: str = "https://drive.google.com/file/d/1-iQk5hnwFMfCljW5JyGpC0w1_jWCGF0z/view?usp=sharing"
+
+
+
+
+"""
+Data Validation realted contant start with DATA_VALIDATION VAR NAME
+"""
+
+DATA_VALIDATION_DIR_NAME: str = "data_validation"
+
+DATA_VALIDATION_STATUS_FILE = 'status.txt'
+
+DATA_VALIDATION_ALL_REQUIRED_FILES = ["train", "valid", "test", "data.yaml"]
